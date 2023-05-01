91475bdbaab35e8c07dacaffab7d173566703456
KlassArtem <1trabl123@gmail.com>
2023-05-01 15:11:09 +0300

    Full done

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,31 @@
+import { IconContext } from 'react-icons';
+import {
+  FooterStyle,
+  FooterLink,
+  LinkedIn,
+  GitHub,
+  FooterText,
+} from './Footer.styled';
+
+export const Footer = () => {
+  return (
+    <FooterStyle>
+      <FooterLink
+        href="https://github.com/KlassArtem"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <IconContext.Provider
+          value={{
+            color: '#777777',
+            size: '25px',
+            style: { verticalAlign: 'middle' },
+          }}
+        >
+          <GitHub />
+        </IconContext.Provider>
+      </FooterLink>
+      <FooterText> &copy; 2023</FooterText>
+    </FooterStyle>
+  );
+};
